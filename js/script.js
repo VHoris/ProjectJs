faecb1f53499ead12a16c7640328a999044ef9f4
Vhoris <vhoris@gmail.com>
2022-03-03 22:26:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//const answer = prompt("Вам 18?","18")
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+///answers[0] = prompt('Як ваше ымя')
+const user ="Ivan";
+
+alert(`Привет, ${user}`);
