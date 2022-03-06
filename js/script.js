abdcb9b07439ec593487a60fae6d6856cbe082d2
Vhoris <vhoris@gmail.com>
2022-03-06 20:43:07 +0200

    new prg

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 "use strict";
 
-//alert('Hello');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//const result = confirm("Are you here?");
-//const answer = prompt("Вам 18?","18")
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const category = 'toys';
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-console.log(`https://someurl.com/${category}/5`);
-///answers[0] = prompt('Як ваше ымя')
-const user ="Ivan";
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-alert(`Привет, ${user}`);
+console.log(personalMovieDB);
